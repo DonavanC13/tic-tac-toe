f8de95864618d56387718dd0fa770450dd1355df
DonavanC13 <donavanc13@outlook.com>
2024-06-25 18:40:48 -0400

    Implemented game board logic for Tic-Tac-Toe game including printing a formatted board to the console and marking cells with player marks

---
+++ script.js
@@ -0,0 +1,25 @@
+const gameBoard = (() => {
+    let board = ['', '', '', '', '', '', '', '', '']
+  
+    const printFormattedBoard = () => {
+      let formattedString = ''
+      board.forEach((cell, index) => {
+        formattedString += cell ? ` ${cell} |` : '   |'
+        if ((index + 1) % 3 === 0) {
+          formattedString = formattedString.slice(0, -1)
+          if (index < 8)
+            formattedString +=
+              '\n\u2015\u2015\u2015 \u2015\u2015\u2015 \u2015\u2015\u2015\n'
+        }
+      })
+      console.log('%c' + formattedString, 'color: #c11dd4;font-size:16px')
+    }
+  
+    const markCell = (cellIndex, mark) => {
+      if (board[cellIndex] === '') {
+        board[cellIndex] = mark
+        return true
+      } else {
+        return false
+      }
+    }
